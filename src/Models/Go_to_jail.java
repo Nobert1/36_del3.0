329e50f488f27b48fb2fd8e7f2112df8c66ec7cf
Nobert1 <s185031@student.dtu.dk>
2018-11-22 17:53:08 +0100

    Game logic, added a board for logic purposes and a polymorfic method in the square class.

--- src/Models/Go_to_jail.java
+++ src/Models/Go_to_jail.java
@@ -1,55 +1,19 @@
 package Models;
-import Controllers.GameBoard;
 import gui_fields.GUI_Player;
 
-/**
- * Very simple class since it only takes the attributes from Field. It is it's own class because it has a unique polymorphic call.
- * Currently having some problems with actually jailling people, seems like Java is a free language.
- * - comment by Gustav
- */
 public class Go_to_jail extends Fields {
 
-
-    private GameBoard gb = GameBoard.getInstance();
     int jailtime;
-    private Player player = gb.getPlayer();
-    private Board fields = gb.getFIELDSINSTANS();
-    private GUI_Player currentGUIPlayer = gb.getCurrentGUIPlayer();
-
-
-
     public Go_to_jail(int position, String name, int jailtime) {
         super(position, name);
         this.jailtime = jailtime;
     }
 
+    public void setJailtime() {
+    }
 
     @Override
     public void FieldFunctionality() {
 
-
-        gb.gui.getFields()[18].setCar(gb.getCurrentGUIPlayer(), false);
-
-        gb.getPlayer().setCurrentPosition(6);
-        gb.getCurrentGUIPlayer().setPlacement(6);
-
-        gb.gui.getFields()[6].setCar(gb.getCurrentGUIPlayer(), true);
-
-        gb.getPlayer().setInJail(true);
-
-
-    }
-
-
-    @Override
-    public void OutputToGUI() {
-
-        gb.gui.showMessage(toString());
-
-    }
-
-    @Override
-    public String toString() {
-        return "You landed on number " + getPosition() + "You are going to jail :/";
     }
 }
